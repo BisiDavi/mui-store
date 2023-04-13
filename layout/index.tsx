53f2a8331d8fc6cc02a2452700aa1b37d89dd18f
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 00:40:59 +0100

    Header, Footer added

---
+++ layout/index.tsx
@@ -0,0 +1,14 @@
+import type { PropsWithChildren } from "react";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
