a47f24368fd3480925c45ea3e89cf895b565586c
BisiDavi <oludavidconnect@gmail.com>
2023-04-14 02:05:33 +0100

    BestSellingProduct added

--- components/BestSellingCategories.tsx
+++ components/BestSellingCategories.tsx
@@ -2,9 +2,9 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Image from "next/image";
+import { Paper } from "@mui/material";
 
 import featured from "@/json/featured.json";
-import { Paper } from "@mui/material";
 
 export default function BestSellingCategories() {
   return (
---
+++ components/BestSellingProducts.tsx
@@ -0,0 +1,16 @@
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Image from "next/image";
+import { Paper } from "@mui/material";
+
+import featured from "@/json/featured.json";
+
+export default function BestSellingProducts() {
+  return (
+    <Box>
+      <Typography>Best Selling Products</Typography>
+      
+    </Box>
+  );
+}
